c6d1033604a991d37cd8618c8bde14a81f742d83
Andres Wolfe <andreswolfe008@gmail.com>
2025-10-31 15:05:35 -0400

    Menu buttons for server

--- scripts/menu/main_menu.gd
+++ scripts/menu/main_menu.gd
@@ -2,6 +2,9 @@ extends Control
 
 signal start_game()
 @onready var buttons_v_box = %ButtonsVBox
+@onready var multiButtons = %MultiButtonsVBox
+@onready var pongButtons = %PongVBox
+@onready var pingButtons = %PingVBox
 
 func _on_start_game_pressed() -> void:
 	start_game.emit()
@@ -19,3 +22,24 @@ func focus_button() -> void:
 		var button : Button = buttons_v_box.get_child(0)
 		if button is Button:
 			button.grab_focus()
+
+
+func _on_multiplayer_pressed() -> void:
+	multiButtons.visible = true
+	buttons_v_box.visible = false
+
+
+func _on_server_button_pressed() -> void:
+	pongButtons.visible = true
+	multiButtons.visible = false
+
+func _on_client_button_pressed() -> void:
+	pingButtons.visible = true
+	multiButtons.visible = false
+
+
+func _on_back_button_pressed() -> void:
+	buttons_v_box.visible = true
+	multiButtons.visible = false
+	pongButtons.visible = false
+	pingButtons.visible = false
